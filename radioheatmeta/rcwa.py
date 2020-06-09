513155c22c5053e26b4dc7655ae3d8dbd540c6d3
Yang Long <longyang_123@yeah.net>
2020-06-09 21:09:14 +0800

    update RCWA

--- radioheatmeta/rcwa.py
+++ radioheatmeta/rcwa.py
@@ -1,2 +1,50 @@
 import numpy as np 
 
+def mesh_grid(vL, vR):
+    qL, qR = np.meshgrid(vL, vR)
+    return qL, qR
+
+def get_S_matrices(start_layer, N, num_of_layer, M_matrices, F_matrices, S_matrices, direction):
+    r1 =0
+    r2 = 2*N - 1
+    r3 = 3*N
+    r4 = 4*N - 1
+
+    if (direction == "all") or (direction == "down"):
+        for i in range(start_layer,0,-1):
+            I = np.linalg.solve(M_matrices[i], M_matrices[i-1])
+            
+            left_top = I[r3:r4, r3:r4]
+            right_top = I[r3:r4, r1:r2]
+            left_bottom = I[r1:r2, r3:r4]
+            right_bottom = I[r1:r2, r1:r2]
+
+            S_matrices[i-1][r1:r2, r1:r2] = np.linalg.solve(
+                left_top - F_matrices[i].dot(S_matrices[i][r1:r2, r3:r4].dot(left_bottom)),
+                F_matrices[i]
+            ).dot(S_matrices[i][r1:r2, r1:r2])
+
+            
+
+def sinc(x):
+    if np.isscalar(x):
+        if x == 0:
+            return 1
+        else:
+            return np.sin(x)/x
+    else:
+        sin_x = np.sin(x)
+        x = np.array(x)
+        positions = np.where(x==0)
+        x[positions] = 1
+        result = sin_x / x
+        result[positions] = 1
+        return result
+
+def jinc(x):
+
+def get_grand_imaginary_matrices(grand_imaginary_matrices, im_eps_xx, im_eps_xy, im_eps_yx, im_eps_yy, im_eps_zz, num_of_layer, N):
+
+def get_E_matrices(E_matrices, eps_xx, eps_xy, eps_yx, eps_yy, num_of_layer, N):
+
+def poynting_flux(omega, thickness_list, kx, ky, E_matrices, grand_imaginary_matrices, eps_zz_inv, Gx_mat, Gy_mat, source_list, target_layer, N, polar, z):
--- radioheatmeta/simulation.py
+++ radioheatmeta/simulation.py
@@ -3,11 +3,11 @@ import numpy as np
 class Simulation:
     def __init__(self, verbose=False):
         self.__num_G = 0
-        self.__omega_list = list()
+        self.__omega_list = None
         self.__num_omega = 0
         self.__target_z = -1
 
-        self.__phi = list()
+        self.__phi = None
         self.__kx_start = 0
         self.__kx_end = 0
         self.__ky_start = 0
@@ -34,7 +34,7 @@ class Simulation:
 
         self.__source_list = None
         self.__thickness_list = None
-        self.__dimension = None
+        self.__dimension = "no"
         self.__options = None
 
     def add_material(self, name, infile):
@@ -62,8 +62,14 @@ class Simulation:
     def set_num_of_G(self, num_G):
 
     def get_phi(self):
+        if self.__phi is None:
+            raise Exception("Phi is None, Please integrate firstly!")
+        return self.__phi
 
     def get_omega(self):
+        if self.__omega_list is None:
+            raise Exception("Omega list is None!")
+        return self.__omega_list
 
     def get_epsilon(self, omege_index, position):
 
@@ -73,6 +79,45 @@ class Simulation:
 
     def init_simulation(self):
 
+    def get_phi_at_kx_ky(self, omega_index, kx, ky=0):
+
+    def get_num_of_G(self):
+
+    def output_sys_info(self):
+
+    def opt_print_intermediate(self, output_flat=""):
+
+    def opt_only_compute_TE(self):
+
+    def opt_only_compute_TM(self):
+
+    def opt_set_lattice_truncation(self, truncation):
+
+    def set_thread(self, num_thread):
+
+    def set_kx_integral(self, points, end=0):
+
+    def set_kx_integral_sym(self, points, end=0):
+
+    def set_ky_integral(self, points, end=0):
+
+    def set_ky_integral_sym(self, points, end=0):
+
+    def integrate_ky_ky(self):
+
+    def integrate_kx_ky_MPI(self, rank, size):
+
+    def integrate_kx_ky_GPU(self):
+
+    def __integrate_kx_ky_internal(self, start, end, parallel=False, rank=0):
+
+    def build_RCWA_matrices(self):
+
+    def reset_simulation(self):
+
+    def set_target_layer_by_layer(self, layer):
+
+    def get_structure(self):
 
         
 class SimulationPlanar(Simulation):
